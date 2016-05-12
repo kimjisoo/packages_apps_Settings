ezio84/purenexus_packages_apps_Settings

SQUASHED Settings: SlimPie (2/2)
Porting from slimkat:
- https://github.com/SlimRoms/packages_apps_Settings/commit/b84104dcbe1b8cd722ee034ae215918ed69ec79b
- switch to existing SlimSeekBarPreference
- adapted to lp5.1 source and the lp5.1 Griffin's custom navbar code

PS: fixed Buttons menu FC

PS: added pie related code from:
https://github.com/SlimRoms/packages_apps_Settings/commit/30482e059fb55b9d3aa4d461a0715d77ec70776a
https://github.com/SlimRoms/packages_apps_Settings/commit/5b08e1f566a2352dbbca325493b7a441a323647b

PS: Pie: checkbox to switchpreference everywhere

PS 9: Use the correct reset icon in pie button style and dimenstion settings

PS10: fixed the trolling backup icon again :D

PS11-12: fixed copyright date
PS13: Rebase, fix conflict

Change-Id: I4f5c57e326687b72892060599a4fd919f1ea658d

Slimpie: button fragment class needs to be declared

src/com/android/settings/SettingsActivity.java
@@ -120,7 +120,9 @@ import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.search.DynamicIndexableContentMonitor;
 import com.android.settings.search.Index;
 import com.android.settings.sim.SimSettings;
+import com.android.settings.slim.fragments.PieButtonFragment;
 import com.android.settings.slim.NavbarSettings;
+import com.android.settings.slim.PieControl;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.users.UserSettings;
 import com.android.settings.vpn2.VpnSettings;
@@ -368,7 +370,9 @@ public class SettingsActivity extends Activity
             ExpandedDesktopPreferenceFragment.class.getName(),
             DisplayRotation.class.getName(),
             BlacklistSettings.class.getName(),
-            NavbarSettings.class.getName()
+            NavbarSettings.class.getName(),
+            PieControl.class.getName(),
+            PieButtonFragment.class.getName()
     };
 
 

kimjisoo/packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -116,6 +116,8 @@ import com.android.settings.print.PrintSettingsFragment;
 import com.android.settings.search.DynamicIndexableContentMonitor;
 import com.android.settings.search.Index;
 import com.android.settings.sim.SimSettings;
+import com.android.settings.slim.fragments.PieButtonFragment;
+import com.android.settings.slim.PieControl;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.users.UserSettings;
 import com.android.settings.vpn2.VpnSettings;
@@ -355,7 +357,9 @@ public class SettingsActivity extends Activity
             ProcessStatsSummary.class.getName(),
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
-            HeadsUpSettings.class.getName()
+            HeadsUpSettings.class.getName(),
+            PieControl.class.getName(),
+            PieButtonFragment.class.getName()
     };
 
 